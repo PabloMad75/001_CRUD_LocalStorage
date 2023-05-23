c64c7ef1da057d98d4b86051d47520567d2b0340
PabloMad75 <pablo.madrid.riquelme@gmail.com>
2023-05-23 17:26:01 -0400

    carga de datos localStorage al Html

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -4,6 +4,7 @@ const addressInput = document.getElementById("direccionInput");
 const mailInput = document.getElementById("mailInput");
 const typeUserInput = document.getElementById("perfilusuario");
 const estateUserInput = document.getElementById("estadousuario");
+const tableBodyInput = document.getElementById("tablebody");
 
 const btnSave = document.getElementById("btn_grabar");
 const btnCancel = document.getElementById("btn_cancelar");
@@ -57,5 +58,68 @@ const getInfoLocalStorage = () => {
 	return JSON.parse(namesString) || [];
 };
 
+/*
+estructura tabla
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+*/
+// Función para Mostrar la lista de nombres en el Html
+const viewData = () => {
+	listHTML.innerHTML = "";
+	const names = getInfoLocalStorage();
+	names.forEach((name) => {
 
+
+		// const listItem = document.createElement("li");
+		// listItem.textContent = name.name;
+		// const btnDelete = document.createElement("button");
+		// btnDelete.textContent = "Borrar";
+		// btnDelete.type = "Button";
+		// btnDelete.id = "btnDelete";
+		// btnDelete.className = "btn__borrar";
+		// const btnEdit = document.createElement("button");
+		// btnEdit.textContent = "Editar";
+		// btnEdit.id = "btnEdit";
+		// btnEdit.className = "btn__Edit";
+		// btnEdit.type = "Button";
+		// listItem.appendChild(btnEdit);
+		// listItem.appendChild(btnDelete);
+		// listHTML.appendChild(listItem);
+
+		// Espera un clic en el boton editar para llamar a la funcion editName
+		btnEdit.addEventListener("click", () => {
+			editName(name.id);
+		});
+		// Espera un clic en el boton Borrar para llamar a la funcion deleteName
+		btnDelete.addEventListener("click", () => {
+			deleteName(name.id);
+		});
+	});
+};
+
+const loadData = ()=>{
+    names=getInfoLocalStorage();
+    tableBodyInput.innerHTML = '';
+    names.forEach(name => {
+    const row = `
+      <tr>
+        <td>${name.name}</td>
+        <td class = colspan="2">${name.address}</td>
+        <td>${name.mail}</td>
+        <td>${name.typeUser}</td>
+        <td>${name.estateUser}</td>
+        <td>
+          <button data-id="${name.id}" class="edit-btn">Editar</button>
+          <button data-id="${name.id}" class="delete-btn">Eliminar</button>
+        </td>
+      </tr>
+    `;
+    tableBodyInput.innerHTML += row;
+
+})
+};
 
