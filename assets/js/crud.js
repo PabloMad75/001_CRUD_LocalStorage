427c93b11b77532061f00ad6e769d641d42f688b
PabloMad75 <pablo.madrid.riquelme@gmail.com>
2023-05-24 00:44:07 -0400

    Pendiente Editar usuarios

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -1,6 +1,5 @@
 const formRegister = document.getElementById("form_register");
 const nameInput = document.getElementById("nombre");
-const addressInput = document.getElementById("direccionInput");
 const mailInput = document.getElementById("mailInput");
 const typeUserInput = document.getElementById("perfilusuario");
 const estateUserInput = document.getElementById("estadousuario");
@@ -9,117 +8,240 @@ const tableBodyInput = document.getElementById("tablebody");
 const btnSave = document.getElementById("btn_grabar");
 const btnCancel = document.getElementById("btn_cancelar");
 const btnUpdate = document.getElementById("btn_actualizar");
-// const listHTML = document.getElementById("nameList");
+
+// Una vez cargado el contenido del DOM, si existe visualizo la info del localstorage
+document.addEventListener("DOMContentLoaded", (e) => {
+  loadData();
+});
 
 btnSave.addEventListener("click", () => {
-    if (nameInput.value.trim() === "" || addressInput.value.trim() === "" 
-    || mailInput.value.trim() === "" || typeUserInput.value.trim() === ""
-    || estateUserInput.value.trim() === "") {
-        console.log("El campo está vacío");
-        Swal.fire({
-            position: 'top-end',
-            icon: 'warning',
-            title: `Debes completar toda la información requerida!`,
-            showConfirmButton: false,
-            timer: 1500
-        });
-    } else {
-        console.log("campos bien");
-        const names = getInfoLocalStorage();
-		const idName = Date.now();
-		const name = {
-			id: idName,
-			name: nameInput.value,			
-            address: addressInput.value,
-			mail: mailInput.value,
-			typeUser: typeUserInput.value,
-			estateUser: estateUserInput.value
-		};
-		names.push(name);
-		localStorage.setItem("names", JSON.stringify(names));
-		formRegister.reset();
+  if (
+    nameInput.value.trim() === "" ||
+    mailInput.value.trim() === "" ||
+    typeUserInput.value.trim() === "" ||
+    estateUserInput.value.trim() === ""
+  ) {
+    warning();
+  } else {
+    console.log("campos bien");
+    const names = getInfoLocalStorage();
+    const idName = Date.now();
+    const name = {
+      id: idName,
+      name: nameInput.value,
+      mail: mailInput.value,
+      typeUser: typeUserInput.value,
+      estateUser: estateUserInput.value,
+    };
+    names.push(name);
+    localStorage.setItem("names", JSON.stringify(names));
+    formRegister.reset();
+    loadData();
+    success();
 
-        // const names = getInfoLocalStorage();
-        // const idName = generateId();
-        // const name = {
-        // 	id: idName,
-        // 	name: nameInput.value,
-        // };
-        // names.push(name);
-        // localStorage.setItem("names", JSON.stringify(names));
-        // form_name.reset();
-    }
-    // viewData();
+
+  }
 });
 
 // Función para obtener la lista de nombres desde el Local Storage
 const getInfoLocalStorage = () => {
-	const namesString = localStorage.getItem("names");
-	return JSON.parse(namesString) || [];
+  const namesString = localStorage.getItem("names");
+  return JSON.parse(namesString) || [];
+};
+
+//Mensaje Sweet Alert Exito
+const success = () =>{
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Los datos fueron grabados exitosamente!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+};
+
+//Mensaje Sweet Alert Error
+const warning= () => {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: `Debes completar la información requerida!`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+};
+
+// función con parametro para borrar elementos del local Storage
+const deleteRecord = (id) => {
+  console.log("funcion delete");
+  const names = ([] = getInfoLocalStorage());
+  names.forEach((name) => {
+    if (name.id === id) {
+      //Elimino del arreglo con splice y findIndex el elemento con el indice entregado por parametro, mostrando la consola
+      console.log(
+        names.splice(
+          names.findIndex((name) => name.id === id),
+          1
+        )
+      );
+      console.log("me encontro");
+
+      //Actualizo la info del localStorage con el arreglo actualizado, sin el elemento eliminado
+      localStorage.setItem("names", JSON.stringify(names));
+
+      //Reseteo el formulario, cargo los botones iniciales del formulario y la funcion viewData actualizar el html
+      formRegister.reset();
+      loadData();
+    }
+  });
+};
+
+const loadData = () => {
+  names = getInfoLocalStorage();
+  tableBodyInput.innerHTML = "";
+  names.forEach((name) => {
+    const row = `
+            <tr>
+              <td class="w-25">${name.name}</td>
+              <td class="w-25">${name.mail}</td>
+              <td class="w-25">${name.typeUser}</td>
+              <td class="">${name.estateUser}</td>
+              <td class="w-15">
+                <button data-id="${name.id}" onclick="editRecord(${name.id})" class="edit-btn btn btn-success btn-sm"><i class="fa-solid fa-pen-to-square"></i> Editar</button>
+                <button data-id="${name.id}" onclick="deleteRecord(${name.id})" class="delete-btn btn btn-danger btn-sm"><i class="fa-solid fa-trash-can"></i> Borrar</button>
+              </td>
+            </tr>
+          `;
+    tableBodyInput.innerHTML += row;
+  });
+};
+
+const activeBtnUpdate = () => {
+  btnSave.classList.remove("active");
+  btnSave.classList.add("inactive");
+  btnUpdate.classList.add("active");
+  btnUpdate.classList.remove("inactive");
+};
+
+const inactiveBtnUpdate = () => {
+  btnSave.classList.add("active");
+  btnSave.classList.remove("inactive");
+  btnUpdate.classList.remove("active");
+  btnUpdate.classList.add("inactive");
+};
+
+// función con parametro para editar elemento del localStorage
+const editRecord = (id) => {
+  const names = getInfoLocalStorage();
+
+  // Buscar el objeto name, mediante la id (id)
+  const nameToUpdate = names.find((name) => name.id === id);
+
+  if (nameToUpdate) {
+    activeBtnUpdate();
+    nombre.value = nameToUpdate.name;
+		mailInput.value = nameToUpdate.mail;
+		perfilusuario.value = nameToUpdate.typeUser;
+		estadousuario.value = nameToUpdate.estateUser;
+
+    // Deshabilitar todos los botones btnEdit
+    const btnEditList = document.getElementsByClassName("edit-btn");
+    for (let i = 0; i < btnEditList.length; i++) {
+      btnEditList[i].disabled = true;
+    }
+
+    // Remover el evento click anterior, si existe, para evitar acumulación de manejadores
+    btnUpdate.removeEventListener("click", updateName);
+
+    // Agregar el nuevo evento click al botón btnUpdate
+    btnUpdate.addEventListener("click", updateName);
+
+    function updateName() {
+      if (
+        nameInput.value.trim() === "" ||
+        mailInput.value.trim() === "" ||
+        typeUserInput.value.trim() === "" ||
+        estateUserInput.value.trim() === ""
+      ){
+        warning();
+        return;
+      } else {
+        nameToUpdate.name = nameInput.value;
+        nameToUpdate.mail = mailInput.value;
+        nameToUpdate.typeUser = typeUserInput.value;
+        nameToUpdate.estateUser = estateUserInput.value;
+        console.log("campo nombre valor: " + nameToUpdate.name);
+        console.log("campo nombre valor: " + nameToUpdate.mail);
+        console.log("campo nombre valor: " + nameToUpdate.typeUser);
+        console.log("campo nombre valor: " + nameToUpdate.estateUser);
+        localStorage.setItem("names", JSON.stringify(names));
+        inactiveBtnUpdate();
+        success();
+      }
+      loadData();
+      // Remover el evento click después de su ejecución
+      btnUpdate.removeEventListener("click", updateName);
+
+      // Mostrar nuevamente los botones btnEdit
+      const btnEditList = document.getElementsByClassName("edit-btn");
+      for (let i = 0; i < btnEditList.length; i++) {
+        btnEditList[i].disabled = false;
+      }
+
+      formRegister.reset();
+      loadData();
+    }
+  }
 };
 
 /*
-estructura tabla
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-*/
-// Función para Mostrar la lista de nombres en el Html
-const viewData = () => {
-	listHTML.innerHTML = "";
+
+const updateRecord =()=> {
+  if (
+    nameInput.value.trim() === "" ||
+    mailInput.value.trim() === "" ||
+    typeUserInput.value.trim() === "" ||
+    estateUserInput.value.trim() === ""
+  ) {
+    console.log("El campo está vacío");
+  } else {
+    names.name = nameInput.value;
+    names.mail = mailInput.value;
+    names.typeUser = typeUserInput.value;
+    names.estateUser = estateUserInput.value;
+    localStorage.setItem("names", JSON.stringify(names));
+    inactiveBtnUpdate();
+  }
+  loadData();
+  // Remover el evento click después de su ejecución
+  btnUpdate.removeEventListener("click", updateRecord);
+  // btnEdit.setAttribute("display", "block");
+  formRegister.reset();
+  inactiveBtnUpdate();
+}
+
+
+// función con parametro para editar elemento del localStorage
+const editRecord = (id) => {
 	const names = getInfoLocalStorage();
-	names.forEach((name) => {
+  activeBtnUpdate();
 
+	// Buscar el objeto name, mediante la id (id)
+	const nameToUpdate = names.find((name) => name.id === id);
 
-		// const listItem = document.createElement("li");
-		// listItem.textContent = name.name;
-		// const btnDelete = document.createElement("button");
-		// btnDelete.textContent = "Borrar";
-		// btnDelete.type = "Button";
-		// btnDelete.id = "btnDelete";
-		// btnDelete.className = "btn__borrar";
-		// const btnEdit = document.createElement("button");
-		// btnEdit.textContent = "Editar";
-		// btnEdit.id = "btnEdit";
-		// btnEdit.className = "btn__Edit";
-		// btnEdit.type = "Button";
-		// listItem.appendChild(btnEdit);
-		// listItem.appendChild(btnDelete);
-		// listHTML.appendChild(listItem);
+	if (nameToUpdate) {
+		document.getElementById("nombre").value = nameToUpdate.name;
+		document.getElementById("mailInput").value = nameToUpdate.mail;
+		document.getElementById("perfilusuario").value = nameToUpdate.typeUser;
+		document.getElementById("estadousuario").value = nameToUpdate.estateUser;
+    console.log("datos de name: "+nameToUpdate);
 
-		// Espera un clic en el boton editar para llamar a la funcion editName
-		btnEdit.addEventListener("click", () => {
-			editName(name.id);
-		});
-		// Espera un clic en el boton Borrar para llamar a la funcion deleteName
-		btnDelete.addEventListener("click", () => {
-			deleteName(name.id);
-		});
-	});
+		//Remover el evento click anterior, si existe, para evitar acumulación de manejadores
+		btnUpdate.removeEventListener("click", updateRecord);
+
+		// Agregar el nuevo evento click al botón btnUpdate
+		btnUpdate.addEventListener("click", updateRecord);
+	}
 };
 
-const loadData = ()=>{
-    names=getInfoLocalStorage();
-    tableBodyInput.innerHTML = '';
-    names.forEach(name => {
-    const row = `
-      <tr>
-        <td>${name.name}</td>
-        <td class = colspan="2">${name.address}</td>
-        <td>${name.mail}</td>
-        <td>${name.typeUser}</td>
-        <td>${name.estateUser}</td>
-        <td>
-          <button data-id="${name.id}" class="edit-btn">Editar</button>
-          <button data-id="${name.id}" class="delete-btn">Eliminar</button>
-        </td>
-      </tr>
-    `;
-    tableBodyInput.innerHTML += row;
-
-})
-};
-
+*/
